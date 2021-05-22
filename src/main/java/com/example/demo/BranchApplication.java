760c44e1c44fe61e46a6a8e105bad2bf86bdbd77
Santhosh <santhosh.naroju5595@gmail.com>
2021-05-22 22:34:09 +0530

    private int variable

--- src/main/java/com/example/demo/BranchApplication.java
+++ src/main/java/com/example/demo/BranchApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BranchApplication {
+private int serv;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BranchApplication.class, args);
