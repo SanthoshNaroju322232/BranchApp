1a9d14e47536ad20f17858633fa0c3726a7acac6
Santhosh <santhosh.naroju5595@gmail.com>
2021-05-22 22:26:48 +0530

    private variable string

--- src/main/java/com/example/demo/BranchApplication.java
+++ src/main/java/com/example/demo/BranchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BranchApplication {
-
+private String Dao;
 	public static void main(String[] args) {
 		SpringApplication.run(BranchApplication.class, args);
 	}
